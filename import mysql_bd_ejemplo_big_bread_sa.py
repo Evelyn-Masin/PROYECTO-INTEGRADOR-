fad53048fb201f2bcf568a333e9afb091d0dd0eb
María Micaela Vilchez Cabrera <129962506+MicaelaVilchez@users.noreply.github.com>
2023-06-11 21:49:20 -0300

    Delete import mysql_bd_ejemplo_big_bread_sa.py
    Reemplazo el archivo de conexión por otro que con la clase Conectar_Base_de_Datos
    
    Signed-off-by: María Micaela Vilchez Cabrera <129962506+MicaelaVilchez@users.noreply.github.com>

--- import mysql_bd_ejemplo_big_bread_sa.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-midb = mysql.connector.connect (
-    host = "localhost",
-    user = "root",
-    password = "Prueba!123",
-    database = "bd_ejemplo_big_bread_sa"
-)
-print(midb)
